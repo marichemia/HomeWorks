d99368652516ed916d4c2abcad0405c5c54edfac
marichemia <marichemia10@gmail.com>
2024-10-05 00:05:34 +0400

    add solution to task 2

--- homework-5/task-1/obj/Debug/net8.0/task-1.AssemblyInfo.cs
+++ homework-5/task-1/obj/Debug/net8.0/task-1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7ba1800e61e9455f492619e19c470ccb151f403")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fda4d120efe618dbb0dbb13d5c12a44bd6536c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("task-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
