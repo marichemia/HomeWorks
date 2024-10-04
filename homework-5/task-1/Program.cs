9fda4d120efe618dbb0dbb13d5c12a44bd6536c3
marichemia <marichemia10@gmail.com>
2024-10-04 23:37:22 +0400

    add solution to task 1

---
+++ homework-5/task-1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+//task 1
+
+Console.WriteLine("Please enter length of the Radius:");
+
+var radius = Convert.ToInt32(Console.ReadLine());
+
+var s2 = Math.Pow(radius * 2, 2);
+var s1 = s2 / 2;
+
+Console.WriteLine($"Difference is {s2-s1}");
+
