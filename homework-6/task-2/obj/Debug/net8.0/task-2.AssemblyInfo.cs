b0f36d6ab5843d1b3f0ab4b3b546f5e26e28ce09
marichemia <marichemia10@gmail.com>
2024-10-08 21:45:06 +0400

    add task 5 solution

--- homework-6/task-2/obj/Debug/net8.0/task-2.AssemblyInfo.cs
+++ homework-6/task-2/obj/Debug/net8.0/task-2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+963d3d0359828281f82d5c2194ffd3609c467103")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff9adb00313f0886c5e48af6530ad556d691b63")]
 [assembly: System.Reflection.AssemblyProductAttribute("task-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ homework-6/task-5/Program.cs
@@ -0,0 +1,25 @@
+﻿//task 5
+
+Console.WriteLine("Enter a number:");
+var input = Console.ReadLine();
+
+ReadDigits(input);
+
+static void ReadDigits (string num) { 
+
+    var converted = Convert.ToInt32(num);
+
+    if (converted > 0 && num.Length == 1) 
+    {
+        Console.WriteLine(num);
+        return; 
+    }  else if (converted < 0){
+        Console.WriteLine("Invalid Parameter");
+        return;
+    }
+
+    Console.WriteLine(num[0]);
+    ReadDigits(num.Substring(1));
+
+
+}
